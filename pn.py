7fc59e564fddb20eb588340f552df46dd5540cba
karthigabanu <53573189+karthigabanu@users.noreply.github.com>
2019-08-02 14:15:17 +0530

    Create pn.py

---
+++ pn.py
@@ -0,0 +1,11 @@
+#Program to check where the number is positive or negative or zero
+a=int(input("number:"))
+if a>0:
+    print("positive")
+elif a==0:
+    print("zero")
+else:
+    print("negative")
+#output:
+#number:-12
+#negative
