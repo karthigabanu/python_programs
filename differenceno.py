c952863d6a4371c9bb50b66f1264b1e890c51229
karthigabanu <53573189+karthigabanu@users.noreply.github.com>
2019-08-04 08:26:02 +0530

    Create differenceno.py

---
+++ differenceno.py
@@ -0,0 +1,6 @@
+#to get the difference between a given number and 17, if the number is greater than 17 return double the absolute difference.
+a=int(input("enter a number:"))
+if a>17:
+    print(2*(a-17))
+else:
+    print(a-17)
