12e3b41f80bcd7c1e01e0006bfe3b41a20256f7e
karthigabanu <53573189+karthigabanu@users.noreply.github.com>
2019-08-04 19:52:50 +0530

    Create contain.py

---
+++ contain.py
@@ -0,0 +1,7 @@
+#Python program to check whether a specified value is contained in a group of values.
+a=[1,6,3,2,7,4,5]
+n=int(input("number:"))
+if n in a:
+    print("true")
+else:
+    print("false")
