933bccfd0b27362f2f1da09caa8d12578f0f1eeb
karthigabanu <53573189+karthigabanu@users.noreply.github.com>
2019-08-04 21:59:30 +0530

    Create print_details.py

---
+++ print_details.py
@@ -0,0 +1,5 @@
+# Python program to display your details like name, age, address in three different lines
+a=input("Name:")
+b=int(input("Age:"))
+c=input("Address:")
+print("Name: {}\nAge: {}\nAddress: {}".format(a, b, c))
