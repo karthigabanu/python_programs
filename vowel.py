5227cebb4dd38b14059ddec97d7605ba07cd3716
karthigabanu <53573189+karthigabanu@users.noreply.github.com>
2019-08-04 08:48:18 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,7 @@
+#test whether a passed letter is a vowel or not.
+a=str(input("enter a string"))
+l=['a','e','i','o','u','A','E','I','O','U']
+if a in l:
+    print("it is a vowel")
+else:
+    print("not a vowel")
