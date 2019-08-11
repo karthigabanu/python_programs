9bad8db573b579cf84652f18fe5dc110220c5483
karthigabanu <53573189+karthigabanu@users.noreply.github.com>
2019-08-11 19:41:30 +0530

    Create rectangle.py

---
+++ rectangle.py
@@ -0,0 +1,10 @@
+class rectangle:
+    def a(self,l,b):
+        self.l=x
+        self.b=y
+        r=x*y
+        return r
+c=rectangle()
+x=int(input("enter the length: "))
+y=int(input("enter the breadth: "))
+print(c.a(x,y))
