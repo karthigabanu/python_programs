cb11df402467c43f5ec1304ea8724e1217134179
KARTHIKEYAN ANBARASU <53579498+karthikeyansa@users.noreply.github.com>
2019-10-25 01:13:31 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -1,9 +1,7 @@
 # Program to check the leap year
 a=int(input("year:"))
-if a%4==0 and a%400==0:
-    print("it is a leap year")
+if ((a % 4 == 0)and (a % 100 != 0) and (a % 400 == 0)):
+    print('leap year:'a)
 else:
-    print("not a leap year")
-#output:
-#year:2000
-#it is a leap year
+    print('not a leap year')
+    
